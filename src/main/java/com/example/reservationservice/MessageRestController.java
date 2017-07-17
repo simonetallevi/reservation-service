85aa4b622367533133d649f017d14a11097261ce
Simone Tallevi-Diotallevi <simone.tallevi@pirelli.com>
2017-07-17 18:36:56 +0200

    sharing project

--- src/main/java/com/example/reservationservice/MessageRestController.java
+++ src/main/java/com/example/reservationservice/MessageRestController.java
@@ -1,5 +1,7 @@
 package com.example.reservationservice;
 
+import io.sentry.Sentry;
+import io.sentry.event.UserBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -19,9 +21,15 @@ public class MessageRestController {
 
     @RequestMapping("/message")
     public String read(){
+
+        Sentry.init();
+        Sentry.getContext().setUser(
+                new UserBuilder().setUsername("user1").build()
+        );
+
         log.error("message : {}", message);
         if(true)
-            throw new IllegalStateException("ERORR");
+            throw new IllegalStateException("ERORR - 2");
         return this.message;
     }
 }
